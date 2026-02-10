5a77bc31bfba4dad4d169d452ee8f3cc5e35b54f
ilnurtuhtarov <something82972@gmail.com>
2026-02-10 05:16:43 +0400

    schemas

---
+++ commerce/shopping-store/src/main/resources/db/schema-init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS products (
+    productId BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    product_name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    image_src VARCHAR,
+    price DECIMAL CHECK (price > 0),
+    quantityState VARCHAR NOT NULL,
+    productState VARCHAR NOT NULL,
+    category VARCHAR NOT NULL
+);
