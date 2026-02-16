a44319f62486854b9e09ee442a2e77d8c39df350
I1nur <tuxtarov0475@gmail.com>
2026-02-16 15:14:36 +0400

    fix: db schema shopping-cart save

--- commerce/shopping-cart/src/main/resources/db/schema-init.sql
+++ commerce/shopping-cart/src/main/resources/db/schema-init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS shopping_carts (
 
 CREATE TABLE IF NOT EXISTS shopping_carts_products (
     shopping_cart_id uuid NOT NULL references shopping_carts(shopping_cart_id),
-    product_id uuid NOT NULL REFERENCES products(id),
+    product_id uuid NOT NULL REFERENCES products(product_id),
     quantity INTEGER NOT NULL,
     PRIMARY KEY (product_id, shopping_cart_id)
 );
